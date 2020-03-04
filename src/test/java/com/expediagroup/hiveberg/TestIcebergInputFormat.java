757d5b9bb48377b35f12fa2772a5f4487306597c
massdosage <awoodhead@expediagroup.com>
2020-03-04 14:52:51 +0000

    fix test in eclipse

--- src/test/java/com/expediagroup/hiveberg/TestIcebergInputFormat.java
+++ src/test/java/com/expediagroup/hiveberg/TestIcebergInputFormat.java
@@ -39,7 +39,7 @@ import com.klarna.hiverunner.StandaloneHiveRunner;
 import com.klarna.hiverunner.annotations.HiveSQL;
 
 @RunWith(StandaloneHiveRunner.class)
-public class TestIcebergInputFormat<T> {
+public class TestIcebergInputFormat {
 
   @HiveSQL(files = {}, autoStart = true)
   private HiveShell shell;
@@ -49,15 +49,14 @@ public class TestIcebergInputFormat<T> {
   @Before
   public void before() throws IOException {
     tableLocation = java.nio.file.Files.createTempDirectory("temp").toFile();
-    Schema schema = new Schema(
-        optional(1, "name", Types.StringType.get()),
-        optional(2, "salary", Types.LongType.get())
-    );
+    Schema schema = new Schema(optional(1, "name", Types.StringType.get()),
+        optional(2, "salary", Types.LongType.get()));
     PartitionSpec spec = PartitionSpec.unpartitioned();
     HadoopTables tables = new HadoopTables();
     Table table = tables.create(schema, spec, tableLocation.getAbsolutePath());
 
-    DataFile fileA = DataFiles.builder(spec)
+    DataFile fileA = DataFiles
+        .builder(spec)
         .withPath("src/test/resources/test-table/data/00000-1-c7557bc3-ae0d-46fb-804e-e9806abf81c7-00000.parquet")
         .withFileSizeInBytes(0)
         .withRecordCount(3) // needs at least one record or else metrics will filter it out
@@ -68,17 +67,19 @@ public class TestIcebergInputFormat<T> {
   @Test
   public void testGetSplits() {
     shell.execute("CREATE DATABASE source_db");
-    shell.execute(new StringBuilder()
-        .append("CREATE TABLE source_db.table_a ")
-        //.append("(name STRING, salary INT) ")
-        .append("ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.avro.AvroSerDe' ")
-        .append("STORED AS ")
-        .append("INPUTFORMAT 'com.expediagroup.hiveberg.IcebergInputFormat' ")
-        .append("OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat' ")
-        .append("LOCATION '")
-        .append(tableLocation.getAbsolutePath())
-        .append("' TBLPROPERTIES ('avro.schema.literal' = '{\"namespace\":\"source_db\",\"type\":\"record\",\"name\":\"table_a\",\"fields\":[{\"name\":\"name\",\"type\":[\"null\",\"string\"],\"default\":null,\"field_id\":1},{\"name\":\"salary\",\"type\":[\"null\",\"long\"],\"default\":null,\"field_id\":2}]}')")
-        .toString());
+    shell
+        .execute(new StringBuilder()
+            .append("CREATE TABLE source_db.table_a ")
+            // .append("(name STRING, salary INT) ")
+            .append("ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.avro.AvroSerDe' ")
+            .append("STORED AS ")
+            .append("INPUTFORMAT 'com.expediagroup.hiveberg.IcebergInputFormat' ")
+            .append("OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat' ")
+            .append("LOCATION '")
+            .append(tableLocation.getAbsolutePath())
+            .append(
+                "' TBLPROPERTIES ('avro.schema.literal' = '{\"namespace\":\"source_db\",\"type\":\"record\",\"name\":\"table_a\",\"fields\":[{\"name\":\"name\",\"type\":[\"null\",\"string\"],\"default\":null,\"field_id\":1},{\"name\":\"salary\",\"type\":[\"null\",\"long\"],\"default\":null,\"field_id\":2}]}')")
+            .toString());
 
     List<Object[]> result = shell.executeStatement("SELECT * FROM source_db.table_a WHERE salary=3000");
   }
